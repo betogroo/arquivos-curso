148964f2d07638113067d1c3a5e8343768244d7e
Beto Garcia <luizhumberto@gmail.com>
2023-01-17 09:33:39 -0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon :to="{ name: 'HomeView' }" icon="mdi-home" />
+      <v-spacer />
+      <div>
+        <v-btn :to="{ name: 'HomeView' }">Home</v-btn>
+        <v-btn :to="{ name: 'AboutView' }">About</v-btn>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
