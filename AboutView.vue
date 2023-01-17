e2adb2fd5631516045f872c12d83dad941d1ac75
Beto Garcia <luizhumberto@gmail.com>
2023-01-17 09:35:21 -0300

    Create AboutView.vue

---
+++ AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="d-flex justify-center fill-height">
+    <v-responsive
+      max-width="460"
+      class="d-flex align-center text-center fill-height"
+    >
+      <v-img cover src="@/assets/logo.svg" />
+
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+
+      <div class="py-14" />
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
