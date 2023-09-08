ab934f4ba7fbc596b07d7d753ce3c25104661e23
Beto Garcia <luizhumberto@gmail.com>
2023-09-08 14:23:37 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,60 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
     'plugin:prettier/recommended',
-    'plugin:vuetify/base'
+    'plugin:vuetify/base',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-   'vue/max-attributes-per-line': [
+    'vue/define-macros-order': [
+      'error',
+      {
+        order: ['defineProps', 'defineEmits'],
+      },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: true,
+      },
+    ],
+    'vue/max-attributes-per-line': [
       'error',
       {
         singleline: {
-          max: 1
+          max: 1,
         },
         multiline: {
-          max: 1
-        }
-      }
+          max: 1,
+        },
+      },
     ],
     'vue/component-tags-order': [
       'error',
       {
-        order: ['script[setup]', 'template', 'style']
-      }
+        order: ['script[setup]', 'template', 'style'],
+      },
     ],
     'vue/component-name-in-template-casing': [
       'error',
@@ -37,13 +62,13 @@ module.exports = {
       {
         registeredComponentsOnly: true,
         ignores: [],
-        globals: ['RouterView', 'RouterLink']
-      }
-    ]
+        globals: ['RouterView', 'RouterLink'],
+      },
+    ],
   },
   settings: {
     'import/resolver': {
-      typescript: {} // this loads <rootdir>/tsconfig.json to eslint
-    }
-  }
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
 }
